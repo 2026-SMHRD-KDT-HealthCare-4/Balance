2aea140ad00129f16594e63cfad7ccda5e476b49
shimueseong <samy010808@gmail.com>
2026-04-08 17:32:18 +0900

    측면측정 추가

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MonitorPage from './pages/MonitorPage';
 import InitialSetupPage from './pages/InitialSetupPage';
+import SideCapturePage from './pages/SideCapturePage';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/setup" element={<InitialSetupPage />} />
         <Route path="/monitor" element={<MonitorPage />} />
         <Route path="/" element={<div>대시보드 (준비 중)</div>} />
+        <Route path="/side" element={<SideCapturePage />} />
       </Routes>
     </Router>
   );
